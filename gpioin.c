0e1b82b7d9698752413b789d1bead1aea6776d61
tokyosuperch <supachan@sfc.wide.ad.jp>
2022-01-21 22:32:21 +0900

    Changed output to Unixtime (nanosec)

--- gpioin.c
+++ gpioin.c
@@ -8,28 +8,11 @@
 #define IN_PORT 20
 #define OUT_PORT 21
 
-struct timespec out_ts;
-struct tm out_tm;
 struct timespec in_ts;
-struct tm in_tm;
-struct timespec offset;
 
 void gettime_in() {
 	clock_gettime(CLOCK_REALTIME, &in_ts);  //時刻の取得
-	localtime_r( &out_ts.tv_sec, &out_tm);
-	localtime_r( &in_ts.tv_sec, &in_tm);
-	printf("in :%d/%02d/%02d %02d:%02d:%02d.%09ld\n",
-		in_tm.tm_year+1900,
-		in_tm.tm_mon+1,
-		in_tm.tm_mday,
-		in_tm.tm_hour,
-		in_tm.tm_min,
-		in_tm.tm_sec,
-		in_ts.tv_nsec);
-}
-
-void gettime_out() {
-	clock_gettime(CLOCK_REALTIME, &out_ts);  //時刻の取得
+	printf("%ld.%09ld\n", in_ts.tv_sec, in_ts.tv_nsec);
 }
 
 int main() {
